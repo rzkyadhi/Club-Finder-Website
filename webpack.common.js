9878a9b55e14d6616a80809446cfcf557a2024bd
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2021-12-01 21:26:12 +0700

    Implement Webpack

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        })
+    ]
+}
